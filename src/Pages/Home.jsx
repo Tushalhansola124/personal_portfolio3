3fd391642d2659ca1199e83a6db3c35bf7ab5eb2
Tushal Hansola <tushalhansola15@gmail.com>
2025-04-30 23:39:10 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ import ScrollVelocity from "/src/Componets/ScrollVelocity.jsx";
 const Home = () => {
   // const resume="https://drive.google.com/file/d/1rpA78aJmeZTS7jr3KnhmUsTUgOsDww-z/view?usp=drive_link";
   // const resume = "public/resume.pdf";
-  http://localhost:5173/resume.pdf
+  
    const resume = " http://localhost:5173/resume.pdf";
 
   const DownloadFile = (url) => {

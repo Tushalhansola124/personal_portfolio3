7ed0034da235e94cc0bc2548effca2589fb70ea8
Tushal <tushalhansola15@gmail.com>
2025-11-30 17:18:37 +0530

    first commit

--- src/Componets/Lightrays.jsx
+++ src/Componets/Lightrays.jsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import { Renderer, Program, Triangle, Mesh } from "ogl";
-import "./LightRays.css";
+import "./Lightrays.css";
 import React from "react";
 
 const DEFAULT_COLOR = "#ffffff";

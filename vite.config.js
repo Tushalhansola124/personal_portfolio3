dcf4e4a49ebbf13758a6b4ef7894af95d59bb585
Tushal <tushalhansola15@gmail.com>
2026-04-18 08:54:33 +0530

    this is vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
 
-import React from 'react'
+// import React from 'react'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+//    build: {
+//     chunkSizeWarningLimit: 1000
+//   }
+// })
+
+
+
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-   build: {
+
+
+  optimizeDeps: {
+    include: ["framer-motion"]
+  },
+  ssr: {
+    noExternal: ["framer-motion"]
+  },
+
+  build: {
     chunkSizeWarningLimit: 1000
   }
 })

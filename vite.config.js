a2cdc0954eacfe880ae55e3ab5883be558e48a5c
Tushal <tushalhansola15@gmail.com>
2026-04-20 14:47:33 +0530

    change the vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
 
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
-//    build: {
-//     chunkSizeWarningLimit: 1000
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [
-    react(),        
     tailwindcss(),
   ],
-  build: {
+   build: {
     chunkSizeWarningLimit: 1000
   }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),        
+//     tailwindcss(),
+//   ],
+//   build: {
+//     chunkSizeWarningLimit: 1000
+//   }
+// })

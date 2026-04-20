8088d7e46b0e006cdd242e72897ce2ead67ba55c
Tushal <tushalhansola15@gmail.com>
2026-04-20 15:03:58 +0530

    change the file vercel json and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
 
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+//    build: {
+//     chunkSizeWarningLimit: 1000
+//   }
+// })
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),        // 🔥 MUST
     tailwindcss(),
   ],
-   build: {
+  base: "./",       // 🔥 IMPORTANT for Vercel
+  build: {
     chunkSizeWarningLimit: 1000
   }
 })
-
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
